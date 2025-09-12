f12c63cc55c93379558a4787b08d3315b5144894
Lyaguh NoteBook <artyom07206@gmail.com>
2025-09-12 18:35:16 +0300

    add table

--- src/pages/Reports/Reports.page.tsx
+++ src/pages/Reports/Reports.page.tsx
@@ -1,19 +1,44 @@
-import { Box, Button, Flex, Paper, Select } from "@mantine/core";
+import { Box, Button, Flex, Paper, Text } from "@mantine/core";
 import Header from "./components/Header";
-import { useState } from "react";
-import { DatePickerInput } from "@mantine/dates";
+import axios from "axios";
+import { API } from "../../app/helpers";
+import { notifications } from "@mantine/notifications";
 
 export default function Reports() {
-  const [dateFrom, setDateFrom] = useState<string | null>();
-  const [dateTo, setDateTo] = useState<string | null>();
-  const [type, setType] = useState<string | null>("Applications");
+  // const [dateFrom, setDateFrom] = useState<string | null>();
+  // const [dateTo, setDateTo] = useState<string | null>();
+  // const [type, setType] = useState<string | null>("Applications");
+
+  const handleClick = () => {
+    axios
+      .get(`${API}/table/export?sortBy=%20%20`)
+      .then(() =>
+        notifications.show({
+          title: "Успешно",
+          message: "Отчет успешно обновлен",
+          position: "bottom-center",
+          color: "green",
+          autoClose: 3000,
+        })
+      )
+      .catch(() =>
+        notifications.show({
+          title: "Ошибка",
+          message: "Что-то пошло не так",
+          position: "bottom-center",
+          color: "red",
+          autoClose: 3000,
+        })
+      );
+  };
+
   return (
     <>
       <Header />
       <Box p={15}>
         <Paper withBorder bg="white" p={15} shadow="xs">
           <Flex direction="column" gap={20}>
-            <Select
+            {/* <Select
               size="lg"
               value={type}
               onChange={setType}
@@ -23,8 +48,8 @@ export default function Reports() {
                 { value: "BoxOffice", label: "Касса" },
               ]}
               label="Вид отчета"
-            />
-            <DatePickerInput
+            /> */}
+            {/* <DatePickerInput
               size="lg"
               value={dateFrom}
               onChange={setDateFrom}
@@ -37,8 +62,13 @@ export default function Reports() {
               onChange={setDateTo}
               label="По"
               placeholder="Выберите конечную дату"
-            />
-            <Button size="lg">Скачать отчет</Button>
+            /> */}
+            <Button size="lg">Сформировать отчет</Button>
+            <Text ta="center" style={{ textDecoration: "underline" }}>
+              <a href="https://docs.google.com/spreadsheets/d/1FYMjDXtYuH3I7nF6WldKTA8pXvzEwQl-JcHn6gLgPew/edit?usp=sharing">
+                Таблица доступна по ссылке
+              </a>
+            </Text>
           </Flex>
         </Paper>
       </Box>
